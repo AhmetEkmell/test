f690fa656aff64de4c4271e6f0dfd0a41d846a47
Ahmet YEŞİLYURT <ekmel.147@gmail.com>
2021-06-24 15:41:17 +0300

    first commit

---
+++ src/layouts/Navi.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import CartSummary from './CartSummary'
+import { Button, Dropdown, Menu, Container } from 'semantic-ui-react'
+
+export default function Navi() {
+    return (
+        <div>
+            <Menu inverted fixed="top">
+                <Container>
+                    <Menu.Item name='home' />
+                    <Menu.Item name='messages' />
+
+                    <Menu.Menu position='right'>
+                        <CartSummary/>
+
+                        <Menu.Item>
+                            <Button primary>Sign Up</Button>
+                        </Menu.Item>
+                    </Menu.Menu>
+                </Container>
+            </Menu>
+        </div>
+    )
+}
